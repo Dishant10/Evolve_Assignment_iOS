e88046087da94c435655ab893cdd5bcdc8230210
Dishant Nagpal <dishantnagpal10@gmail.com>
2024-11-29 23:23:42 +0530

    Initial Commit

---
+++ Evolve_iOS_Assignment/Evolve_iOS_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Evolve_iOS_AssignmentApp.swift
+//  Evolve_iOS_Assignment
+//
+//  Created by Dishant Nagpal on 29/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Evolve_iOS_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
